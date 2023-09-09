2a90a446425a0f3c13b74d1e8ca9867d50f0bec1
AxelMoraCastanon <89556262+AxelMoraCastanon@users.noreply.github.com>
2023-09-08 23:26:40 -0700

    Add files via upload

---
+++ GymPulse/GymPulse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GymPulse
+//
+//  Created by Axel Mora on 9/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
