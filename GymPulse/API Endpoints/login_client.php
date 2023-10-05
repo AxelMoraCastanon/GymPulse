1799ac17f9f875414514fbffeb392b86706a81ea
Axel Mora <axelmora2011@gmail.com>
2023-10-05 11:36:54 -0700

    Updated login_client & trainer.php API endpoints

--- GymPulse/API Endpoints/login_client.php
+++ GymPulse/API Endpoints/login_client.php
@@ -3,17 +3,21 @@ include 'db_connection.php';
 
 $data = json_decode(file_get_contents("php://input"));
 
+// Check if email and password are set
 if(isset($data->email) && isset($data->password)){
-    $stmt = $pdo->prepare("SELECT password FROM clients WHERE email = ?");
+    $stmt = $pdo->prepare("SELECT client_id, first_name, last_name, password FROM clients WHERE email = ?");
     $stmt->execute([$data->email]);
     $user = $stmt->fetch();
-    
-    if(password_verify($data->password, $user['password'])){
-        echo json_encode(["message" => "Login successful"]);
+
+    // Check if user exists and password is correct
+    if($user && password_verify($data->password, $user['password'])){
+        // Unset password before sending user data
+        unset($user['password']);
+        echo json_encode(['status' => 'success', 'user' => $user]);
     } else {
-        echo json_encode(["message" => "Invalid credentials"]);
+        echo json_encode(['status' => 'error', 'message' => 'Invalid credentials']);
     }
 } else {
-    echo json_encode(["message" => "Invalid input"]);
+    echo json_encode(['status' => 'error', 'message' => 'Invalid input']);
 }
 ?>
--- GymPulse/API Endpoints/login_trainer.php
+++ GymPulse/API Endpoints/login_trainer.php
@@ -3,17 +3,21 @@ include 'db_connection.php';
 
 $data = json_decode(file_get_contents("php://input"));
 
+// Check if email and password are set
 if(isset($data->email) && isset($data->password)){
-    $stmt = $pdo->prepare("SELECT password FROM trainers WHERE email = ?");
+    $stmt = $pdo->prepare("SELECT trainer_id, first_name, last_name, password FROM trainers WHERE email = ?");
     $stmt->execute([$data->email]);
     $trainer = $stmt->fetch();
-    
-    if(password_verify($data->password, $trainer['password'])){
-        echo json_encode(["message" => "Login successful"]);
+
+    // Check if trainer exists and password is correct
+    if($trainer && password_verify($data->password, $trainer['password'])){
+        // Unset password before sending trainer data
+        unset($trainer['password']);
+        echo json_encode(['status' => 'success', 'trainer' => $trainer]);
     } else {
-        echo json_encode(["message" => "Invalid credentials"]);
+        echo json_encode(['status' => 'error', 'message' => 'Invalid credentials']);
     }
 } else {
-    echo json_encode(["message" => "Invalid input"]);
+    echo json_encode(['status' => 'error', 'message' => 'Invalid input']);
 }
 ?>
