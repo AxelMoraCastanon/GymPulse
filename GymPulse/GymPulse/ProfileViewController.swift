938cb97595e3a9d411ea8726e73d8b78d5c325cb
Axel Mora <axelmora2011@gmail.com>
2023-10-06 23:36:09 -0700

    Fixed ProfileViewController

--- GymPulse/GymPulse/ProfileViewController.swift
+++ GymPulse/GymPulse/ProfileViewController.swift
@@ -1,3 +1,4 @@
+// ProfileViewController.swift
 import UIKit
 
 class ProfileViewController: UIViewController {
@@ -9,6 +10,8 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var passwordTF: UITextField!
     @IBOutlet weak var gymNameTF: UITextField!
     @IBOutlet weak var gymAddressTF: UITextField!
+    @IBOutlet weak var verifyInfoButton: UIButton!
+    @IBOutlet weak var updateButton: UIButton!
     
     let baseURL = "http://ec2-54-219-186-173.us-west-1.compute.amazonaws.com/"
     var isClient: Bool = true
@@ -24,29 +27,194 @@ class ProfileViewController: UIViewController {
         tapGesture.cancelsTouchesInView = false
         view.addGestureRecognizer(tapGesture)
         
-        setTextFieldEditableState(to: false)
-        fetchAndDisplayUserDetails()
+        // Initialize the state of the text fields and buttons
+        initializeProfileState()
     }
 
-    // Method to hide keyboard
     @objc func hideKeyboard() {
         view.endEditing(true)
     }
 
-    
-    @IBAction func updateButtonPressed(_ sender: UIButton) {
-        setTextFieldEditableState(to: true)
+    func initializeProfileState() {
+        // Clear all text fields
+        firstNameTF.text = ""
+        lastNameTF.text = ""
+        emailTF.text = ""
+        phoneNumberTF.text = ""
+        passwordTF.text = ""
+        gymNameTF.text = ""
+        gymAddressTF.text = ""
         
-        let alert = UIAlertController(title: "Update Profile", message: "Do you want to update your profile?", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { _ in
-            self.updateUserProfile()
+        // Disable all text fields initially
+        setTextFieldEditableState(to: false)
+        
+        // Disable email and gymName text fields
+        emailTF.isUserInteractionEnabled = false
+        gymNameTF.isUserInteractionEnabled = false
+        
+        // Grey out and disable the verifyInfo and update buttons
+        verifyInfoButton.isEnabled = false
+        verifyInfoButton.alpha = 0.5
+        updateButton.isEnabled = false
+        updateButton.alpha = 0.5
+        
+        // Display the role selection alert
+        displayUserRoleAlert()
+    }
+
+    func displayUserRoleAlert() {
+        let alert = UIAlertController(title: "Select Role", message: "Type an email if you are a client or an email and gym name if you are a trainer.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Client", style: .default, handler: { _ in
+            self.isClient = true
+            self.emailTF.isUserInteractionEnabled = true
+            self.verifyInfoButton.isEnabled = true
         }))
-        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Trainer", style: .default, handler: { _ in
+            self.isClient = false
+            self.emailTF.isUserInteractionEnabled = true
+            self.gymNameTF.isUserInteractionEnabled = true
+            self.verifyInfoButton.isEnabled = true
+        }))
         self.present(alert, animated: true, completion: nil)
     }
 
     @IBAction func logoutButtonPressed(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
+    }
+
+    
+    @IBAction func verifyInfoButtonPressed(_ sender: UIButton) {
+        if isClient {
+            // Fetch client_id using get_client_id.php
+            guard let email = emailTF.text, !email.isEmpty else {
+                displayError(message: "Please enter an email.")
+                return
+            }
+            
+            let endpoint = "get_client_id.php?email=\(email)"
+            fetchData(endpoint: endpoint) { response in
+                if let id = response["client_id"] as? Int {
+                    self.clientId = id
+                    self.displayUpdateInformationAlert()
+                    
+                    // Enable the update button if any of the text fields have content
+                    if !(self.firstNameTF.text?.isEmpty ?? true) || !(self.lastNameTF.text?.isEmpty ?? true) || !(self.emailTF.text?.isEmpty ?? true) || !(self.phoneNumberTF.text?.isEmpty ?? true) || !(self.passwordTF.text?.isEmpty ?? true) {
+                        self.updateButton.isEnabled = true
+                        self.updateButton.alpha = 1.0
+                    }
+                } else {
+                    self.displayError(message: "No client found with the provided email.")
+                }
+            }
+        } else {
+            // Fetch trainer_id using get_trainer_id.php and location_id using get_location_id.php
+            guard let email = emailTF.text, !email.isEmpty, let gymName = gymNameTF.text, !gymName.isEmpty else {
+                displayError(message: "Please enter both email and gym name.")
+                return
+            }
+            
+            let trainerEndpoint = "get_trainer_id.php?email=\(email)"
+            fetchData(endpoint: trainerEndpoint) { response in
+                if let id = response["trainer_id"] as? Int {
+                    self.trainerId = id
+                    
+                    let locationEndpoint = "get_location_id.php?trainer_id=\(id)"
+                    self.fetchData(endpoint: locationEndpoint) { locationResponse in
+                        if let locationId = locationResponse["location_id"] as? Int {
+                            self.locationId = locationId
+                            self.displayUpdateInformationAlert()
+                            
+                            // Enable the update button if any of the text fields have content
+                            if !(self.firstNameTF.text?.isEmpty ?? true) || !(self.lastNameTF.text?.isEmpty ?? true) || !(self.emailTF.text?.isEmpty ?? true) || !(self.phoneNumberTF.text?.isEmpty ?? true) || !(self.gymNameTF.text?.isEmpty ?? true) || !(self.gymAddressTF.text?.isEmpty ?? true) || !(self.passwordTF.text?.isEmpty ?? true) {
+                                self.updateButton.isEnabled = true
+                                self.updateButton.alpha = 1.0
+                            }
+                        } else {
+                            self.displayError(message: "No location found for the provided gym name.")
+                        }
+                    }
+                } else {
+                    self.displayError(message: "No trainer found with the provided email.")
+                }
+            }
+        }
+    }
+
+    func displayUpdateInformationAlert() {
+        let alert = UIAlertController(title: "Update Information", message: "You can now update your information.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+            self.setTextFieldEditableState(to: true)
+            self.updateButton.isEnabled = true
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    @IBAction func updateButtonPressed(_ sender: UIButton) {
+        if isClient {
+            // Call update_client.php to update the client's information
+            guard let clientId = clientId else {
+                displayError(message: "Client ID not found.")
+                return
+            }
+            
+            let endpoint = "update_client.php"
+            let parameters: [String: Any] = [
+                "client_id": clientId,
+                "first_name": firstNameTF.text ?? "",
+                "last_name": lastNameTF.text ?? "",
+                "email": emailTF.text ?? "",
+                "phone_number": phoneNumberTF.text ?? "",
+                "password": passwordTF.text ?? ""
+            ]
+            
+            sendDataToAPI(endpoint: endpoint, parameters: parameters) { success in
+                if success {
+                    print("Client profile updated successfully.")
+                } else {
+                    print("Failed to update client profile.")
+                }
+            }
+        } else {
+            // Call update_trainer.php and/or update_location.php based on the fields filled
+            guard let trainerId = trainerId, let locationId = locationId else {
+                displayError(message: "Trainer ID or Location ID not found.")
+                return
+            }
+            
+            let trainerEndpoint = "update_trainer.php"
+            let trainerParameters: [String: Any] = [
+                "trainer_id": trainerId,
+                "first_name": firstNameTF.text ?? "",
+                "last_name": lastNameTF.text ?? "",
+                "email": emailTF.text ?? "",
+                "phone_number": phoneNumberTF.text ?? "",
+                "location_id": locationId,
+                "password": passwordTF.text ?? ""
+            ]
+            
+            sendDataToAPI(endpoint: trainerEndpoint, parameters: trainerParameters) { success in
+                if success {
+                    print("Trainer profile updated successfully.")
+                    
+                    let locationEndpoint = "update_location.php"
+                    let locationParameters: [String: Any] = [
+                        "location_id": locationId,
+                        "gym_name": self.gymNameTF.text ?? "",
+                        "address": self.gymAddressTF.text ?? ""
+                    ]
+                    
+                    self.sendDataToAPI(endpoint: locationEndpoint, parameters: locationParameters) { locationSuccess in
+                        if locationSuccess {
+                            print("Location details updated successfully.")
+                        } else {
+                            print("Failed to update location details.")
+                        }
+                    }
+                } else {
+                    print("Failed to update trainer profile.")
+                }
+            }
+        }
     }
 
     func setTextFieldEditableState(to state: Bool) {
@@ -58,6 +226,7 @@ class ProfileViewController: UIViewController {
         gymNameTF.isUserInteractionEnabled = !isClient && state
         gymAddressTF.isUserInteractionEnabled = !isClient && state
     }
+
 
     func fetchAndDisplayUserDetails() {
         if isClient, let clientId = clientId {
