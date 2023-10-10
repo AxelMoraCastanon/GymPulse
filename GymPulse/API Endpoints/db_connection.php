f8dd05f573c54bc03c209acf01fa2d33c27bc43b
Axel Mora <axelmora2011@gmail.com>
2023-10-09 19:47:11 -0700

    Updated db_connection.php  for SSL Certificate

--- GymPulse/API Endpoints/db_connection.php
+++ GymPulse/API Endpoints/db_connection.php
@@ -12,6 +12,7 @@ $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::MYSQL_ATTR_SSL_CA       => '/etc/ssl/certs/rds-combined-ca-bundle.pem' // Add this line for SSL
 ];
 
 try {
