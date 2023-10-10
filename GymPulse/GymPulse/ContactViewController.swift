cae5dcd38a0e7ca5cd33593dbebbd792709d5531
Axel Mora <axelmora2011@gmail.com>
2023-10-10 00:38:13 -0700

    Added MapKit View

--- GymPulse/GymPulse/ContactViewController.swift
+++ GymPulse/GymPulse/ContactViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class ContactViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
@@ -17,17 +18,23 @@ class ContactViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var viewTrainersButton: UIButton!
     @IBOutlet weak var trainerPickerView: UIPickerView!
-
-    var gyms: [String] = []
-    var selectedGymInfo: [String: Any] = [:]
-    var trainers: [[String: Any]] = []
+    
+    private let baseURL = "http://ec2-54-219-186-173.us-west-1.compute.amazonaws.com/"
+    
+    private var gyms: [String] = []
+    private var selectedGymInfo: [String: Any] = [:]
+    private var trainers: [[String: Any]] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        fetchGymNames()
+    }
+
+    private func setupUI() {
         gymPickerView.delegate = self
         gymPickerView.dataSource = self
-        gymAddressTextField.isUserInteractionEnabled = false // Ensure the user cannot edit the text field
-        fetchGymNames()
+        gymAddressTextField.isUserInteractionEnabled = false
     }
 
     @IBAction func selectGymPressed(_ sender: UIButton) {
@@ -37,57 +44,115 @@ class ContactViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
     @IBAction func viewTrainersPressed(_ sender: UIButton) {
         if let locationId = selectedGymInfo["location_id"] as? Int {
             fetchTrainers(by: locationId)
+        } else {
+            showAlert(title: "Error", message: "Location ID not found.")
         }
     }
 
-    func fetchGymNames() {
+    // MARK: - Networking
+
+    private func fetchGymNames() {
         let url = URL(string: baseURL + "get_all_gym_names.php")!
-        URLSession.shared.dataTask(with: url) { data, _, _ in
-            if let data = data, let gymNames = try? JSONDecoder().decode([String].self, from: data) {
-                DispatchQueue.main.async {
-                    self.gyms = gymNames
-                    self.gymPickerView.reloadAllComponents()
-                }
-            }
-        }.resume()
+        fetchData(from: url) { (data: [String]?) in
+            self.gyms = data ?? []
+            self.gymPickerView.reloadAllComponents()
+        }
     }
 
-    func fetchGymInfo(by name: String) {
-        let url = URL(string: baseURL + "get_gym_info_by_gym_name.php?gym_name=\(name)")!
-        URLSession.shared.dataTask(with: url) { data, _, _ in
-            if let data = data, let gymInfo = try? JSONDecoder().decode([String: Any].self, from: data) {
-                DispatchQueue.main.async {
-                    self.selectedGymInfo = gymInfo
-                    self.gymAddressTextField.text = gymInfo["address"] as? String
-                    // Add code to update the MKMapView with the gym's address
+    private func fetchGymInfo(by name: String) {
+        let encodedName = name.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        let url = URL(string: baseURL + "get_gym_info_by_gym_name.php?gym_name=\(encodedName)")!
+        fetchData(from: url) { (data: [String: Any]?) in
+            if let gymInfo = data {
+                self.selectedGymInfo = gymInfo
+                if let address = gymInfo["address"] as? String {
+                    self.gymAddressTextField.text = address
+                    self.updateMapView(with: address)
                 }
             }
-        }.resume()
+        }
     }
 
-    func fetchTrainers(by locationId: Int) {
+    private func fetchTrainers(by locationId: Int) {
         let url = URL(string: baseURL + "get_trainer_by_location_id.php?location_id=\(locationId)")!
-        URLSession.shared.dataTask(with: url) { data, _, _ in
-            if let data = data, let trainersInfo = try? JSONDecoder().decode([[String: Any]].self, from: data) {
+        fetchData(from: url) { (data: [[String: Any]]?) in
+            self.trainers = data ?? []
+            self.trainerPickerView.reloadAllComponents()
+        }
+    }
+
+    private func fetchData<T>(from url: URL, completion: @escaping (T?) -> Void) where T: Any {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Error fetching data: \(error.localizedDescription)")
                 DispatchQueue.main.async {
-                    self.trainers = trainersInfo
-                    self.trainerPickerView.reloadAllComponents()
+                    self.showAlert(title: "Error", message: "Failed to fetch data. Please try again.")
                 }
+                return
+            }
+            
+            guard let data = data else {
+                print("No data received.")
+                return
+            }
+            
+            do {
+                if T.self is [String].Type {
+                    let decodedData = try JSONDecoder().decode([String].self, from: data)
+                    DispatchQueue.main.async {
+                        completion(decodedData as? T)
+                    }
+                } else if T.self is [String: Any].Type || T.self is [[String: Any]].Type {
+                    if let jsonObject = try JSONSerialization.jsonObject(with: data, options: []) as? T {
+                        DispatchQueue.main.async {
+                            completion(jsonObject)
+                        }
+                    }
+                }
+            } catch {
+                print("Error decoding JSON: \(error.localizedDescription)")
             }
         }.resume()
     }
 
-    // UIPickerView DataSource and Delegate methods
+    // MARK: - UI Helpers
+
+    private func updateMapView(with address: String) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            if let error = error {
+                print("Error geocoding address: \(error.localizedDescription)")
+                return
+            }
+            
+            if let placemark = placemarks?.first, let location = placemark.location {
+                let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+                self.mapView.setRegion(region, animated: true)
+                
+                // Add a pin to the map
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = location.coordinate
+                annotation.title = self.selectedGymInfo["gym_name"] as? String
+                annotation.subtitle = address
+                self.mapView.addAnnotation(annotation)
+            }
+        }
+    }
+
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    // MARK: - UIPickerView DataSource and Delegate
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
 
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if pickerView == gymPickerView {
-            return gyms.count
-        } else {
-            return trainers.count
-        }
+        return pickerView == gymPickerView ? gyms.count : trainers.count
     }
 
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
--- GymPulse/GymPulse/trainerInformationController.swift
+++ GymPulse/GymPulse/trainerInformationController.swift
@@ -13,12 +13,18 @@ class trainerInformationController: UIViewController {
     @IBOutlet weak var lastNameTF: UITextField!
     @IBOutlet weak var emailTF: UITextField!
     @IBOutlet weak var phoneNumberTF: UITextField!
-
+    
+    private let baseURL = "http://ec2-54-219-186-173.us-west-1.compute.amazonaws.com/"
+    
     var trainerInfo: [String: Any] = [:]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTextFields()
         populateTrainerInfo()
+    }
+
+    private func setupTextFields() {
         // Ensure the user cannot edit the text fields
         firstNameTF.isUserInteractionEnabled = false
         lastNameTF.isUserInteractionEnabled = false
@@ -26,10 +32,41 @@ class trainerInformationController: UIViewController {
         phoneNumberTF.isUserInteractionEnabled = false
     }
 
-    func populateTrainerInfo() {
-        firstNameTF.text = trainerInfo["first_name"] as? String
-        lastNameTF.text = trainerInfo["last_name"] as? String
-        emailTF.text = trainerInfo["email"] as? String
-        phoneNumberTF.text = trainerInfo["phone_number"] as? String
+    private func populateTrainerInfo() {
+        // Populate the text fields with the trainer's information
+        if let firstName = trainerInfo["first_name"] as? String {
+            firstNameTF.text = firstName
+        } else {
+            handleMissingInfoError(key: "first_name")
+        }
+        
+        if let lastName = trainerInfo["last_name"] as? String {
+            lastNameTF.text = lastName
+        } else {
+            handleMissingInfoError(key: "last_name")
+        }
+        
+        if let email = trainerInfo["email"] as? String {
+            emailTF.text = email
+        } else {
+            handleMissingInfoError(key: "email")
+        }
+        
+        if let phoneNumber = trainerInfo["phone_number"] as? String {
+            phoneNumberTF.text = phoneNumber
+        } else {
+            handleMissingInfoError(key: "phone_number")
+        }
+    }
+
+    private func handleMissingInfoError(key: String) {
+        print("Error: \(key) not found in trainerInfo.")
+        showAlert(title: "Error", message: "Missing information for \(key). Please contact support.")
+    }
+
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
