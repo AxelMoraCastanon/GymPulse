c8457cd418f7275cb13ecd2a89dca209e698f1a3
AxelMoraCastanon <89556262+AxelMoraCastanon@users.noreply.github.com>
2023-11-30 22:47:36 -0800

    Delete GymPulse/API Endpoints/secrets.php

--- GymPulse/API Endpoints/secrets.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Keep your Stripe API key protected by including it as an environment variable
-// or in a private script that does not publicly expose the source code.
-
-// This is your test secret API key.
-$stripeSecretKey = 'sk_test_51OBkFgHHUyq7ejf5jLxpTfIFwb4UPx62fggmJ1PhPHBIf3tpu0yYk9vYcKoJGyjDBC082H95uVCOcU9cSakpGuTZ00QRbVnpEJ';
